33aadbe996a9b42886b7dd9a1002008a47ce3d07
Alyssa Falcione <lyzarddz@gmail.com>
2022-01-12 14:05:05 -0500

    Completed

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+const totalBatteries = batteryBatches.reduce(addTogether)
+
+function addTogether(num, total){
+    return num + total;
+}
+console.log(batteryBatches.reduce(addTogether));
